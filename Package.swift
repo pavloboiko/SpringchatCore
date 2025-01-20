1df5be8728cc9c58bd602042b0a28fbbc4b2fcb4
Pavlo Boiko <boiko.impul@gmail.com>
2025-01-20 04:29:49 -0500

    Update

--- Package.swift
+++ Package.swift
@@ -9,19 +9,208 @@ let package = Package(
     products: [
         .library(
             name: "SpringchatCoreFramework",
-            targets: ["SpringchatCoreFramework"]
+            targets: ["SpringchatCoreFramework", "Chatto", "Thirdparty"]
+        ),
+        .library(
+            name: "SpringchatCore",
+            targets: ["SpringchatCore"]
         ),
     ],
     dependencies: [
         .package(
+            url: "https://github.com/marmelroy/PhoneNumberKit",
+            exact: "3.7.5"
+        ),
+        .package(
+            url: "https://github.com/Alamofire/Alamofire.git",
+            .upToNextMajor(from: "5.9.1")
+        ),
+        .package(
+            url: "https://github.com/mixpanel/mixpanel-swift",
+            branch: "master"
+        ),
+        .package(
             url: "https://github.com/realm/realm-swift",
             branch: "master"
+        ),
+        .package(
+            url: "https://github.com/RxSwiftCommunity/RxDataSources",
+            .upToNextMajor(from: "5.0.2")
+        ),
+        .package(
+            url: "https://github.com/RxSwiftCommunity/RxRealm",
+            .upToNextMajor(from: "5.0.6")
+        ),
+        .package(
+            url: "https://github.com/ReactiveX/RxSwift",
+            .upToNextMajor(from: "6.6.0")
+        ),
+        .package(
+            url: "https://github.com/Stefterv/filament_swift/",
+            exact: "1.52.0"
+        ),
+        .package(
+            url: "https://github.com/ChartsOrg/Charts",
+            .upToNextMajor(from: "5.1.0")
+        ),
+        .package(
+            url: "https://github.com/khanlou/Promise",
+            .upToNextMajor(from: "2.0.4")
+        ),
+        .package(
+            url: "https://github.com/swiftcsv/SwiftCSV",
+            .upToNextMajor(from: "0.8.0")
+        ),
+        .package(
+            url: "https://github.com/airbnb/lottie-ios",
+            exact: "3.1.9"
+        ),
+        .package(
+            url: "https://github.com/kean/Nuke",
+            exact: "7.6.3"
+        ),
+        .package(
+            url: "https://github.com/devxoul/Toaster",
+            branch: "master"
+        ),
+        .package(
+            url: "https://github.com/zekunyan/TTGTagCollectionView",
+            branch: "master"
+        ),
+        .package(
+            url: "https://github.com/Orderella/PopupDialog",
+            branch: "master"
+        ),
+        .package(
+            url: "https://github.com/SnapKit/SnapKit",
+            .upToNextMajor(from: "5.0.1")
+        ),
+        .package(
+            url: "https://github.com/youtube/youtube-ios-player-helper",
+            .upToNextMajor(from: "1.0.4")
+        ),
+        .package(
+            url: "https://github.com/alexiscreuzot/SwiftyGif",
+            branch: "master"
+        ),
+        .package(
+            url: "https://github.com/socketio/socket.io-client-swift",
+            branch: "master"
+        ),
+        .package(
+            url: "https://github.com/RevenueCat/purchases-ios",
+            .upToNextMajor(from: "5.12.0")
         ),
     ],
     targets: [
         .binaryTarget(
             name: "SpringchatCoreFramework",
             path: "./SpringchatCoreFramework.xcframework"
-        )
+        ),
+        .binaryTarget(
+            name: "Chatto",
+            path: "./Chatto.xcframework"
+        ),
+        .binaryTarget(
+            name: "Thirdparty",
+            path: "./Thirdparty.xcframework"
+        ),
+        .target(
+            name: "SpringchatCore",
+            dependencies: [
+                .product(
+                    name: "PhoneNumberKit",
+                    package: "PhoneNumberKit"
+                ),
+                .product(
+                    name: "Alamofire",
+                    package: "Alamofire"
+                ),
+                .product(
+                    name: "DGCharts",
+                    package: "Charts"
+                ),
+                .product(
+                    name: "Promise",
+                    package: "Promise"
+                ),
+                .product(
+                    name: "RealmSwift",
+                    package: "realm-swift"
+                ),
+                .product(
+                    name: "SwiftCSV",
+                    package: "SwiftCSV"
+                ),
+                .product(
+                    name: "Mixpanel",
+                    package: "mixpanel-swift"
+                ),
+                .product(
+                    name: "RxSwift",
+                    package: "RxSwift"
+                ),
+                .product(
+                    name: "RxCocoa",
+                    package: "RxSwift"
+                ),
+                .product(
+                    name: "RxRealm",
+                    package: "RxRealm"
+                ),
+                .product(
+                    name: "RxDataSources",
+                    package: "RxDataSources"
+                ),
+                .product(
+                    name: "Lottie",
+                    package: "lottie-ios"
+                ),
+                .product(
+                    name: "filament",
+                    package: "filament_swift"
+                ),
+                .product(
+                    name: "Nuke",
+                    package: "Nuke"
+                ),
+                .product(
+                    name: "Toaster",
+                    package: "Toaster"
+                ),
+                .product(
+                    name: "TTGTags",
+                    package: "TTGTagCollectionView"
+                ),
+                .product(
+                    name: "PopupDialog",
+                    package: "PopupDialog"
+                ),
+                .product(
+                    name: "SnapKit",
+                    package: "SnapKit"
+                ),
+                .product(
+                    name: "YouTubeiOSPlayerHelper",
+                    package: "youtube-ios-player-helper"
+                ),
+                .product(
+                    name: "SwiftyGif",
+                    package: "SwiftyGif"
+                ),
+                .product(
+                    name: "SocketIO",
+                    package: "socket.io-client-swift"
+                ),
+                .product(
+                    name: "RevenueCat",
+                    package: "purchases-ios"
+                ),
+                .target(name: "SpringchatCoreFramework"),
+                .target(name: "Thirdparty"),
+                .target(name: "Chatto")
+            ],
+            path: "Sources/Dependencies"
+        ),
     ]
 )
---
+++ Sources/Dependencies/SpringchatCoreDependencies.swift
@@ -0,0 +1,8 @@
+//
+//  SpringchatCoreDependencies.swift
+//
+//
+//  Created by Pavlo Boiko on 1/20/25.
+//
+
+import Foundation
